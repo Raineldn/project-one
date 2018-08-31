36d239bd704ceb0f08b38e910ae4f738f61a746f
Raine <raineldn@gmail.com>
2018-08-31 02:23:06 +0100

    Got the getRandomQuote function working!

--- js/script.js
+++ js/script.js
@@ -2,9 +2,25 @@
 
 // Create the array of quote objects and name it quotes
 
+var quotes = [
+{
+	quote: '"Only I can change my life. No one can do it for me."', 
+	source: 'Carol Burnett'
+},
+{
+	quote: '"The way to get started is to quit talking and begin doing."',
+	source: 'Walt Disney'
+}
+]
 
 
-// Create the getRandomQuuote function and name it getRandomQuote
+// Create the getRandomQuote function and name it getRandomQuote
+
+function getRandomQuote(array) {
+	var randomNumber = Math.floor(Math.random() * quotes.length); // To generate a random number
+	var randomQuote = quotes[randomNumber]; // The randomNumber var is then used to generate a random quote.
+	return randomQuote;
+}
 
 
 
@@ -14,4 +30,4 @@
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+// document.getElementById('loadQuote').addEventListener("click", printQuote, false);
