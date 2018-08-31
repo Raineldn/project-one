41b2f9a3a5494300f2c777cfd6d9ca970bb3f4b0
Raine <raineldn@gmail.com>
2018-08-31 16:53:37 +0100

    Added tag property + updated CSS

--- js/script.js
+++ js/script.js
@@ -7,74 +7,89 @@ var quotes = [
 	quote: 'You\'re gonna need a bigger boat.', 
 	source: 'Brody',
 	date: 1975,
-	citation: 'Jaws'
+	citation: 'Jaws',
+	tag: 'Movies'
 },
 {
 	quote: 'The way to get started is to quit talking and begin doing.',
-	source: 'Walt Disney'
+	source: 'Walt Disney',
+	tag: 'Famous People'
 },
 {
 	quote: 'Never forget what you are, for surely the world will not. Make it your strength. Then it can never be your weakness. Armour yourself in it, and it will never be used to hurt you.',
 	source: 'George R.R. Martin',
 	date: 1996,
-	citation: 'A Game of Thrones'
+	citation: 'A Game of Thrones',
+	tag: 'TV'
 },
 {
 	quote: 'A reader lives a thousand lives before he dies, said Jojen. The man who never reads lives only one.',
 	source: 'George R. R. Martin',
 	date: 2011,
-	citation: 'A Dance with Dragons'
+	citation: 'A Dance with Dragons',
+	tag: 'Literature'
 },
 {
 	quote: 'It\'s the possibility of having a dream come true that makes life interesting.',
 	source: 'Paulo Coelho',
 	date: 1988,
-	citation: 'The Alchemist'
+	citation: 'The Alchemist',
+	tag: 'Literature'
 },
 {
 	quote: 'It\’s no use going back to yesterday, because I was a different person then.',
 	source: 'Lewis Carroll',
 	date: 1865,
-	citation: 'Alice\'s Adventures in Wonderland'
+	citation: 'Alice\'s Adventures in Wonderland',
+	tag: 'Literature'
 },
 {
 	quote: 'The best time to plant a tree was 20 years ago. The second best time is now.',
-	source: 'Chinese Proverb'
+	source: 'Chinese Proverb',
+	tag: 'Proverbs'
 },
 {
 	quote: 'You can never cross the ocean until you have the courage to lose sight of the shore.',
-	source: 'Christopher Colombus'
+	source: 'Christopher Colombus',
+	tag: 'Famous People'
 },
 {
 	quote: 'The best revenge is massive success.',
-	source: 'Frank Sinatra'
+	source: 'Frank Sinatra',
+	tag: 'Famous People'
 },
 {
 	quote: 'Strive not to be a success, but rather to be of value.',
-	source: 'Albert Einstein'
+	source: 'Albert Einstein',
+	tag: 'Famous People'
 },
 {
 	quote: 'The only way to do great work is to love what you do.',
 	source: 'Steve Jobs',
+	tag: 'Famous People'
 },
 {
 	quote: 'Choose a job you love, and you will never have to work a day in your life.',
-	source: 'Confucius'
+	source: 'Confucius',
+	tag: 'Famous People'
 },
 {
 	quote: 'Don\'t be pushed around by the fears in your mind. Be led by the dreams in your heart.',
 	source: 'Roy T. Bennett',
 	date: 2016,
-	citation: 'The Light in the Heart'
+	citation: 'The Light in the Heart',
+	tag: 'Literature'
 },
 {
 	quote: 'Wubba Lubba Dub Dub!',
 	source: 'Rick',
-	citation: 'Rick and Morty'
+	citation: 'Rick and Morty',
+	tag: 'TV'
 },
 {
 	quote: 'Let us be well persuaded that everyone of us possesses happiness in proportion to his virtue and wisdom, and according as he acts in obedience to their suggestion.',
 	source: 'Aristotle',
+	tag: 'Famous People'
 }
 ]
 
@@ -87,23 +102,45 @@ function getRandomQuote(array) {
 	return randomQuote;
 }
 
+// Random background color function (with CREDIT to www.w3resource.com)
+// Found here: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
 
+function backgroundColor() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+}
+  
 
 // Create the printQuote function and name it printQuote
 
+// An empty string which will be filled with the next var.
+
 function printQuote() {
-	var message = ""; // An empty string which will be filled with the next var.
+	var message = ""; 
 	var quoteString = getRandomQuote(quotes);
+	backgroundColor();
 	message = '<p class="quote">' + quoteString.quote + '</p>';
 	message += '<p class="source">' + quoteString.source;
 
-	if ("citation" in quoteString) { // A conditional for a quote citation.
+	// A conditional for a quote citation.
+
+	if ("citation" in quoteString) {
 	message += '<span class="citation">' + quoteString.citation + '</span>';
 	} 
 
-	if ("date" in quoteString) { // A conditional for a quote date.
-	message += '<span class="year">' + quoteString.date + '</span>' + '</p>';
+	// A conditional for a quote date.
+
+	if ("date" in quoteString) { 
+	message += '<span class="year">' + quoteString.date + '</span>';
 	}
+
+	// A conditional for a quote tag (CSS added to styles.css)
+
+	if ("tag" in quoteString) { 
+	message += '<span class="tag">' + quoteString.tag + '</p>';
+	}	
 
 	document.getElementById('quote-box').innerHTML = message;
 }
