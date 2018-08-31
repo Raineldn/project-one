272756c4bdfbde8c5d45634592383a823bef80bf
Raine <raineldn@gmail.com>
2018-08-31 14:19:56 +0100

    Added quotes to the project

--- js/script.js
+++ js/script.js
@@ -4,12 +4,77 @@
 
 var quotes = [
 {
-	quote: "Only I can change my life. No one can do it for me.", 
-	source: 'Carol Burnett'
+	quote: 'You\'re gonna need a bigger boat.', 
+	source: 'Brody',
+	date: 1975,
+	citation: 'Jaws'
 },
 {
-	quote: "The way to get started is to quit talking and begin doing.",
+	quote: 'The way to get started is to quit talking and begin doing.',
 	source: 'Walt Disney'
+},
+{
+	quote: 'Never forget what you are, for surely the world will not. Make it your strength. Then it can never be your weakness. Armour yourself in it, and it will never be used to hurt you.',
+	source: 'George R.R. Martin',
+	date: 1996,
+	citation: 'A Game of Thrones'
+},
+{
+	quote: 'A reader lives a thousand lives before he dies, said Jojen. The man who never reads lives only one.',
+	source: 'George R. R. Martin',
+	date: 2011,
+	citation: 'A Dance with Dragons'
+},
+{
+	quote: 'It\'s the possibility of having a dream come true that makes life interesting.',
+	source: 'Paulo Coelho',
+	date: 1988,
+	citation: 'The Alchemist'
+},
+{
+	quote: 'It\’s no use going back to yesterday, because I was a different person then.',
+	source: 'Lewis Carroll',
+	date: 1865,
+	citation: 'Alice\'s Adventures in Wonderland'
+},
+{
+	quote: 'The best time to plant a tree was 20 years ago. The second best time is now.',
+	source: 'Chinese Proverb'
+},
+{
+	quote: 'You can never cross the ocean until you have the courage to lose sight of the shore.',
+	source: 'Christopher Colombus'
+},
+{
+	quote: 'The best revenge is massive success.',
+	source: 'Frank Sinatra'
+},
+{
+	quote: 'Strive not to be a success, but rather to be of value.',
+	source: 'Albert Einstein'
+},
+{
+	quote: 'The only way to do great work is to love what you do.',
+	source: 'Steve Jobs',
+},
+{
+	quote: 'Choose a job you love, and you will never have to work a day in your life.',
+	source: 'Confucius'
+},
+{
+	quote: 'Don\'t be pushed around by the fears in your mind. Be led by the dreams in your heart.',
+	source: 'Roy T. Bennett',
+	date: 2016,
+	citation: 'The Light in the Heart'
+},
+{
+	quote: 'Wubba Lubba Dub Dub!',
+	source: 'Rick',
+	citation: 'Rick and Morty'
+},
+{
+	quote: 'Let us be well persuaded that everyone of us possesses happiness in proportion to his virtue and wisdom, and according as he acts in obedience to their suggestion.',
+	source: 'Aristotle',
 }
 ]
 
@@ -31,8 +96,14 @@ function printQuote() {
 	var quoteString = getRandomQuote(quotes);
 	message = '<p class="quote">' + quoteString.quote + '</p>';
 	message += '<p class="source">' + quoteString.source;
+
+	if ("citation" in quoteString) {
 	message += '<span class="citation">' + quoteString.citation + '</span>';
-	message += '<span class="year">' + quoteString.year + '</span>' + '</p>';
+	} 
+
+	if ("date" in quoteString) {
+	message += '<span class="year">' + quoteString.date + '</span>' + '</p>';
+	}
 
 	document.getElementById('quote-box').innerHTML = message;
 }
