25c0017e94d1e38251e37bc03034fb352a779369
Raine <raineldn@gmail.com>
2018-08-31 12:29:59 +0100

    Got printQuote function working!

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@
 
 var quotes = [
 {
-	quote: '"Only I can change my life. No one can do it for me."', 
+	quote: "Only I can change my life. No one can do it for me.", 
 	source: 'Carol Burnett'
 },
 {
-	quote: '"The way to get started is to quit talking and begin doing."',
+	quote: "The way to get started is to quit talking and begin doing.",
 	source: 'Walt Disney'
 }
 ]
@@ -24,8 +24,20 @@ function getRandomQuote(array) {
 
 
 
-// Create the printQuote funtion and name it printQuote
+// Create the printQuote function and name it printQuote
 
+function printQuote() {
+	var message = ""; // An empty string which will be filled with the next var.
+	var quoteString = getRandomQuote(quotes);
+	message = '<p class="quote">' + quoteString.quote + '</p>';
+	message += '<p class="source">' + quoteString.source;
+	message += '<span class="citation">' + quoteString.citation + '</span>';
+	message += '<span class="year">' + quoteString.year + '</span>' + '</p>';
+
+	document.getElementById('quote-box').innerHTML = message;
+}
+
+printQuote();
 
 
 // This event listener will respond to "Show another quote" button clicks
